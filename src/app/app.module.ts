5ada875c652cc8a14c89708410694627abdc83f3
Hardy Lutula <hardylutula@gmail.com>
2025-05-19 15:32:34 +0200

    hdjhdshjdsjhgds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TabsComponent } from './components/tabs/tabs.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { ButtonComponent } from './components/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    TabsComponent,
+    LayoutComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
