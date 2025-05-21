41f566b986eed1275a276f5a9614786931de28d1
Hardy Lutula <hardylutula@gmail.com>
2025-05-21 14:11:39 +0200

    components page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { InfoComponent } from './pages/info/info.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/countries', pathMatch: 'full' },
+  { path: 'countries', component: HomePageComponent },
+  { path: 'countries/:countryName', component: InfoComponent },
+  { path: '404', component: NotfoundComponent },
+  { path: '**', redirectTo: '/404', pathMatch: 'full' },
+];
